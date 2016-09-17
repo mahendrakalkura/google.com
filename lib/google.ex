b9b62a59749d07461f27df146615035253cc4409
Mahendra Kalkura <mahendra@kalkura.com>
2016-09-17 17:22:56 +0530

    Simplify functions

--- lib/google.ex
+++ lib/google.ex
@@ -5,6 +5,47 @@ defmodule Google do
   require URI
 
   def query(key, source, target, q) do
+    result = get_arguments(key, source, target, q)
+    result = http_poison(result)
+    result = parse_response(result)
+    result = parse_body(result)
+    result
+  end
+
+  def http_poison(result) do
+    result = HTTPoison.request(result["method"], result["url"], result["body"], result["headers"], result["options"])
+    result
+  end
+
+  def parse_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    result = JSX.decode(body)
+    result
+  end
+
+  def parse_response({:ok, %HTTPoison.Response{status_code: status_code}}) do
+    result = {:error, status_code}
+    result
+  end
+
+  def parse_response({:error, %HTTPoison.Error{reason: reason}}) do
+    result = {:error, reason}
+    result
+  end
+
+  def parse_body({:ok, body}) do
+    data = body["data"]
+    translations = data["translations"]
+    translation = Enum.at(translations, 0)
+    result = {:ok, translation["translatedText"]}
+    result
+  end
+
+  def parse_body({:error, reason}) do
+    result = {:error, reason}
+    result
+  end
+
+  def get_arguments(key, source, target, q) do
     method = :get
     url = "https://www.googleapis.com/language/translate/v2"
     body = ""
@@ -18,36 +59,13 @@ defmodule Google do
     options = [
       {:params, params},
     ]
-    result = parse_http(HTTPoison.request(method, url, body, headers, options))
-    result
-  end
-
-  def parse_http({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
-    result = JSX.decode(body)
-    result = parse_json(result)
-    result
-  end
-
-  def parse_http({:ok, %HTTPoison.Response{status_code: status_code}}) do
-    result = {:error, status_code}
-    result
-  end
-
-  def parse_http({:error, %HTTPoison.Error{reason: reason}}) do
-    result = {:error, reason}
-    result
-  end
-
-  def parse_json({:ok, body}) do
-    data = body["data"]
-    translations = data["translations"]
-    translation = Enum.at(translations, 0)
-    result = {:ok, translation["translatedText"]}
-    result
-  end
-
-  def parse_json({:error, reason}) do
-    result = {:error, reason}
+    result = %{
+      "method" => method,
+      "url" => url,
+      "body" => body,
+      "headers" => headers,
+      "options" => options,
+    }
     result
   end
 end
